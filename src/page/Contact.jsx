062f3f1e88d5e83f7735f69a43946b694f9159e6
minjun <min2jn99@gmail.com>
2022-08-24 22:55:49 +0700

    Add contact page

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -2,8 +2,53 @@ import React from "react";
 
 const Contact = () => {
     return (
-        <div>
-            <div>Contact</div>
+        <div className="contact section">
+            <div className="contact--container">
+                <div className="section--heading text-uppercase mx-auto">
+                    contact
+                </div>
+                <form className="contact--form form flow">
+                    <div className="form--group">
+                        <label htmlFor="yourName" className="form--label">
+                            your name
+                        </label>
+                        <input
+                            type="text"
+                            id="yourName"
+                            className="form--input"
+                            placeholder=" "
+                        />
+                    </div>
+                    <div className="form--group">
+                        <label htmlFor="yourEmail" className="form--label">
+                            your email
+                        </label>
+                        <input
+                            type="email"
+                            id="yourEmail"
+                            className="form--input"
+                            placeholder=" "
+                        />
+                    </div>
+                    <div className="form--group">
+                        <label htmlFor="yourMessage" className="form--label">
+                            your message
+                        </label>
+                        <textarea
+                            id="yourMessage"
+                            className="form--input"
+                            placeholder=" "
+                        ></textarea>
+                    </div>
+                    <button
+                        className="btn"
+                        button-type="primary"
+                        button-variant="contained"
+                    >
+                        contact me
+                    </button>
+                </form>
+            </div>
         </div>
     );
 };
